db0a44aacf340ff18f6573ab80beed090cbdfe21
Leeyaoi <darya.ya2608@gmail.com>
2024-07-01 14:32:03 +0300

    added possibility to choose a chat

--- src/shared/components/modules/ChatListModule/ChatListModule.tsx
+++ src/shared/components/modules/ChatListModule/ChatListModule.tsx
@@ -1,21 +1,33 @@
 import ChatComponent from "../../components/ChatComponent/ChatComponent";
 import SearchBar from "../../UI/SearchBar/SearchBar";
 import AddChatComponent from "../../components/AddChatComponent/AddChatComponent";
+import { useState } from "react";
 import "./ChatListModule.scss";
 
 const ChatListModule = () => {
+  const [selectedIndex, SetSelectedIndex] = useState(-1);
+
   return (
     <>
       <ul className="MainPage_ChatList">
         <SearchBar placeholder="Search chat" />
         <AddChatComponent />
-        <li>
+        <li
+          className={selectedIndex === 0 ? "active" : ""}
+          onClick={() => SetSelectedIndex(0)}
+        >
           <ChatComponent />
         </li>
-        <li>
+        <li
+          className={selectedIndex === 1 ? "active" : ""}
+          onClick={() => SetSelectedIndex(1)}
+        >
           <ChatComponent />
         </li>
-        <li>
+        <li
+          className={selectedIndex === 2 ? "active" : ""}
+          onClick={() => SetSelectedIndex(2)}
+        >
           <ChatComponent />
         </li>
       </ul>
