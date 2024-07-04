c6a830c5d2a11bf136113214b59dd73049cb83ee
Leeyaoi <darya.ya2608@gmail.com>
2024-07-04 15:44:15 +0300

    fixed according to comments

--- src/modules/ChatListModule/ChatListModule.tsx
+++ src/modules/ChatListModule/ChatListModule.tsx
@@ -1,41 +1,45 @@
 import ChatComponent from "../../components/ChatComponent/ChatComponent";
 import SearchBar from "../../shared/UI/SearchBar/SearchBar";
 import AddChatComponent from "../../components/AddChatComponent/AddChatComponent";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./ChatListModule.scss";
 import { List, ListItem } from "@mui/material";
-import { useChatState } from "../../shared/states/ChatStates";
+import { useChatState } from "../../shared/state/ChatSlice";
 import ShortChatType from "../../shared/types/ShortChatType";
 
 const ChatListModule = () => {
-  const [selectedChat, SetSelectedChat] = useChatState((state) => [
-    state.currentChat,
-    state.updateCurrentChat,
-  ]);
-  const [filteredChatList, SetFilteredChatList] = useChatState((state) => [
-    state.filteredChats,
-    state.setFilteredChats,
-  ]);
-  const chatList = useChatState((state) => state.chats);
-  const fetch = useChatState((state) => state.fetchChats);
+  const { currentChat, updateCurrentChat, chats, fetchChats } = useChatState(
+    (state) => ({
+      currentChat: state.currentChat,
+      updateCurrentChat: state.updateCurrentChat,
+      chats: state.chats,
+      fetchChats: state.fetchChats,
+    })
+  );
+
+  const [filteredChatList, SetFilteredChatList] =
+    useState<ShortChatType[]>(chats);
 
   useEffect(() => {
-    fetch();
-  }, [fetch]);
+    fetchChats();
+  }, [fetchChats]);
+
+  console.log(chats);
+  console.log(filteredChatList);
 
   return (
     <>
       <List className="MainPage_ChatList">
         <SearchBar
           placeholder="Search chat"
-          list={chatList}
+          list={chats}
           SetSearchResults={SetFilteredChatList}
         />
         <AddChatComponent />
         {filteredChatList.map((item: ShortChatType) => (
           <ListItem
-            className={selectedChat === item.id ? "activeItem" : ""}
-            onClick={() => SetSelectedChat(item.id)}
+            className={currentChat === item.id ? "activeItem" : ""}
+            onClick={() => updateCurrentChat(item.id)}
             key={item.id}
           >
             <ChatComponent chatName={item.name} />
---
+++ src/shared/state/ChatSlice.ts
@@ -0,0 +1,50 @@
+import { create } from 'zustand'
+import ShortChatType from '../types/ShortChatType';
+import client from '../helpers/client';
+
+interface ChatSlice {
+    loading: boolean,
+    success: boolean,
+    errorMessage: string,
+    chats: ShortChatType[],
+    currentChat: string,
+    updateCurrentChat: (id: string) => void,
+    fetchChats: () => void,
+    createChat: (ChatName: string) => void
+  }
+
+export const useChatState = create<ChatSlice>((set, get) => ({
+    loading: false,
+    success: false,
+    errorMessage: "",
+    chats: [],
+    currentChat: "",
+    updateCurrentChat: (id: string) => { set({ currentChat: id }) },
+    fetchChats: async () => {
+        set({ loading: true });
+        try {
+            const res = await client.get("/Chat");
+            if (res.status >= 400) {
+                set({ errorMessage: res.statusText, loading: false })
+                return;
+            }
+            set({ success: true, chats: res.data, loading: false });
+        } catch (error) {
+            set({ errorMessage: error, loading: false })
+        }        
+    },
+    createChat: async (ChatName: string) => {
+        set({ loading: true });
+        
+        try {
+            const res = await client.post("/Chat", { name: ChatName });
+            if (res.status >= 400) {
+                set({ errorMessage: res.statusText, loading: false })
+                return;
+            }
+            set({ success: true, chats: [...get().chats, res.data], loading: false });
+        } catch (error) {
+            set({ errorMessage: error, loading: false })
+        }
+    }
+}));
