7668b6699934b9f65e329832e5962b65337ca036
Leeyaoi <darya.ya2608@gmail.com>
2024-07-05 22:30:29 +0300

    slightly fixed design

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,10 +6,10 @@ import "./MainPage.scss";
 const MainPage = () => {
   return (
     <Grid container className="Container">
-      <Grid item xs={4}>
+      <Grid item xs={3}>
         <ChatListModule />
       </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={9}>
         <ChatModule />
       </Grid>
     </Grid>
